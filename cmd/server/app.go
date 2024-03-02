d3d06305f15fba623e520c50ccb4b03ec20fa6a8
Daniil <drystunn@gmail.com>
2024-03-02 19:57:47 +0300

    routes

--- cmd/server/app.go
+++ cmd/server/app.go
@@ -1,6 +1,21 @@
 package server
 
-func Run() error {
+import (
+	"github.com/gin-gonic/gin"
+	"shortUrl/shorten_url/internal/repository"
+	"shortUrl/shorten_url/internal/service"
+	"shortUrl/shorten_url/pkg/http"
+)
 
-	return nil
+func Run() error {
+	r := gin.Default()
+
+	urlRepo := repository.NewInMemoryURLRepository()
+	urlService := service.NewUrlRepository(urlRepo)
+	urlHandler := http.NewURLHandler(urlService)
+
+	r.POST("/shorten", urlHandler.ShortenURL)
+	r.GET(":id", urlHandler.RedirectURL)
+
+	return r.Run(":8080")
 }
