3028fc5c44d068968c62ff1e517d8145a3906ec5
Daniil <drystunn@gmail.com>
2024-03-04 15:01:01 +0300

    added counter

--- internal/repository/url_repository.go
+++ internal/repository/url_repository.go
@@ -1,28 +1,41 @@
 package repository
 
-import "shortUrl/shorten_url/internal/domain"
+import (
+	"shortUrl/shorten_url/internal/domain"
+	"sync"
+)
 
 type URLRepository interface {
 	Create(url *domain.URL) error
-	Get(id string) (*domain.URL, error)
+	Get(id int) (*domain.URL, error)
 }
 
 type InMemoryURLRepository struct {
-	urls map[string]*domain.URL
+	urls    map[int]*domain.URL
+	counter int
+	mu      sync.Mutex
 }
 
 func NewInMemoryURLRepository() *InMemoryURLRepository {
 	return &InMemoryURLRepository{
-		urls: make(map[string]*domain.URL),
+		urls:    make(map[int]*domain.URL),
+		counter: 1,
 	}
 }
 
 func (r *InMemoryURLRepository) Create(url *domain.URL) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	url.ID = r.counter
+
 	r.urls[url.ID] = url
+	r.counter++
+
 	return nil
 }
 
-func (r *InMemoryURLRepository) Get(id string) (*domain.URL, error) {
+func (r *InMemoryURLRepository) Get(id int) (*domain.URL, error) {
 	url, ok := r.urls[id]
 	if !ok {
 		return nil, nil
