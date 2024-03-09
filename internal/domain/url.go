ff79b8bd646666afd3cc4393763e3edb0199951f
Daniil <drystunn@gmail.com>
2024-03-09 14:27:51 +0300

    changed URL structure

--- internal/domain/url.go
+++ internal/domain/url.go
@@ -3,6 +3,7 @@ package domain
 import "time"
 
 type URL struct {
+	ID        int       `json:"id"`
 	Original  string    `json:"original"`
 	Shortened string    `json:"shortened"`
 	CreatedAt time.Time `json:"created_at"`
