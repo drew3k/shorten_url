7fb9115784893f93f9f1d78524de67d6fa135c56
Daniil <drystunn@gmail.com>
2024-03-09 18:33:45 +0300

    updated funcs

--- internal/repository/url_repository.go
+++ internal/repository/url_repository.go
@@ -7,7 +7,6 @@ import (
 
 type URLRepository interface {
 	Create(url *domain.URL) error
-	Get(shortened string) (*domain.URL, error)
 }
 
 type InMemoryURLRepository struct {
@@ -27,15 +26,4 @@ func (r *InMemoryURLRepository) Create(url *domain.URL) error {
 
 	r.urls[url.Shortened] = url
 	return nil
-}
-
-func (r *InMemoryURLRepository) Get(shortened string) (*domain.URL, error) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	url, ok := r.urls[shortened]
-	if !ok {
-		return nil, nil
-	}
-	return url, nil
 }
