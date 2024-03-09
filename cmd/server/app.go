bf6a9f5ed268965025228b03edc3be83c50b95e5
Daniil <drystunn@gmail.com>
2024-03-09 15:50:40 +0300

    updated bot logic

--- cmd/server/app.go
+++ cmd/server/app.go
@@ -1,28 +1,20 @@
 package server
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"shortUrl/shorten_url/internal/repository"
 	"shortUrl/shorten_url/internal/service"
 	"shortUrl/shorten_url/pkg/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"github.com/skip2/go-qrcode"
 )
 
 func Run() error {
-	//if err := godotenv.Load(); err != nil {
-	//	log.Fatalf("Failed to load .env file: %v", err)
-	//}
-
-	//db, err := pgx.Connect(context.Background(), fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
-	//	os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
-	//	os.Getenv("DB_NAME"), os.Getenv("SSL_MODE")))
-	//if err != nil {
-	//	return fmt.Errorf("failed to connect to database: %v", err)
-	//}
-	//defer db.Close(context.Background())
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Ошибка при загрузке файла .env:", err)
 	}
@@ -49,8 +41,6 @@ func Run() error {
 	r.POST("/shorten", urlHandler.ShortenURL)
 	r.GET("/shorten/:id", urlHandler.GetURL)
 
-	return r.Run(":8080")
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
@@ -65,14 +55,58 @@ func Run() error {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message.Text != "" {
+			if update.Message.IsCommand() {
+				switch update.Message.Command() {
+				case "start":
+					// Отправляем сообщение с кнопками
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите действие:")
+					msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
+						tgbotapi.NewKeyboardButtonRow(
+							tgbotapi.NewKeyboardButton("Сократить ссылку"),
+						),
+						tgbotapi.NewKeyboardButtonRow(
+							tgbotapi.NewKeyboardButton("Сгенерировать QR-код"),
+						),
+					)
+					bot.Send(msg)
+				//case "short":
+				//	qrCodeFilePath := "qr_code.png"
+				//	qrCodeMsg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, qrCodeFilePath)
+				//	bot.Send(qrCodeMsg)
+				default:
+					// Отвечаем на неизвестные команды
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не знаю такой команды.")
+					bot.Send(msg)
+				}
+			} else {
+				// Проверяем, является ли сообщение URL
+				if strings.HasPrefix(update.Message.Text, "http://") || strings.HasPrefix(update.Message.Text, "https://") {
+					// Создаем сокращенную ссылку
+					shortenedURL, err := urlService.Create(update.Message.Text)
+					if err != nil {
+						log.Printf("Ошибка при сокращении URL: %v", err)
+						continue
+					}
 
-		// Создаем ответное сообщение
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Я бот, который отвечает на все твои сообщения.")
+					// Генерируем QR-код для сокращенной ссылки
+					qrCodeFilePath := "qr_code.png"
+					err = qrcode.WriteFile(shortenedURL.Shortened, qrcode.Medium, 256, qrCodeFilePath)
+					if err != nil {
+						log.Printf("Ошибка при генерации QR-кода: %v", err)
+						continue
+					}
 
-		// Отправляем ответное сообщение
-		if _, err := bot.Send(msg); err != nil {
-			log.Printf("Ошибка при отправке сообщения: %v", err)
+					// Отправляем сокращенную ссылку и QR-код в ответ
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Сокращенная ссылка: "+shortenedURL.Shortened)
+					bot.Send(msg)
+
+				} else {
+					// Отвечаем на сообщение бота
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Введите ссылку для сокращения. Пример: https:// или http://")
+					bot.Send(msg)
+				}
+			}
 		}
 	}
 
