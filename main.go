0c94e0edc61441afb0434b9491ee5a22ff9644c4
Daniil <drystunn@gmail.com>
2024-03-11 13:00:14 +0300

    updated run

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 	"shortUrl/shorten_url/cmd/server"
+	"shortUrl/shorten_url/internal/telegram"
 )
 
 func main() {
-	s := server.NewServer()
+	bot := telegram.NewBotAPI(nil)
+	err := bot.Initialize()
+	if err != nil {
+		log.Fatal("Failed to initialize bot:", err)
+	}
+
+	s := server.NewServer(bot)
 	if err := s.Run(); err != nil {
 		log.Fatal("Failed to start server", err)
 	}
