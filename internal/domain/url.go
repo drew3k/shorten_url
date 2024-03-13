f25b83ecc9466162e6787df6dc45bbdc09200619
Daniil <drystunn@gmail.com>
2024-03-13 23:45:07 +0300

    URLs list struct

--- internal/domain/url.go
+++ internal/domain/url.go
@@ -1,10 +1,11 @@
 package domain
 
-import "time"
-
 type URL struct {
-	ID        int       `json:"id"`
-	Original  string    `json:"original"`
-	Shortened string    `json:"shortened"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int    `json:"id"`
+	Original  string `json:"original"`
+	Shortened string `json:"shortened"`
+}
+
+type ShortenedURLList struct {
+	URLs []URL
 }
